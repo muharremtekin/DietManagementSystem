67831821b28f91fcca8c64102591a550baa9c941
Muharrem Tekin <muharremtekinn@outlook.com.tr>
2025-04-05 23:11:13 +0300

    Add extension method for MediatR registration
    Introduce AddApplicationResgistration method in RegistrationExtensions class to extend IServiceCollection. This method registers MediatR services from the current assembly, facilitating their addition to the dependency injection container in ASP.NET Core applications.

---
+++ src/DietManagementSystem.Application/Extensions/RegistrationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DietManagementSystem.Application.Extensions;
+public static class RegistrationExtensions
+{
+    public static IServiceCollection AddApplicationResgistration(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssemblies(assembly);
+        });
+
+        return services;
+    }
+}
+
