c1a8b3fa2a177b3d967dc834600e5c67168adc1b
Muharrem Tekin <muharremtekinn@outlook.com.tr>
2025-04-09 11:46:58 +0300

    Add constants and new project for common utilities
    - Added `RoleConstants` class in `DietManagementSystem.Common.Constants` namespace with constants for `Admin`, `Client`, and `Dietitian`.
    - Added `RouteConstants` class in `DietManagementSystem.Common.Constants` namespace with constants for various API routes.
    - Created `DietManagementSystem.Common.csproj` targeting .NET 8.0, with implicit usings and nullable reference types enabled.

---
+++ src/DietManagementSystem.Common/Constants/RoleConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace DietManagementSystem.Common.Constants;
+public sealed class RoleConstants
+{
+    public const string Admin = "Admin";
+    public const string Client = "Client";
+    public const string Dietitian = "Dietitian";
+}
+
---
+++ src/DietManagementSystem.Common/Constants/RouteConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace DietManagementSystem.Common.Constants;
+public class RouteConstants
+{
+    public const string admin = "api/admins";
+    public const string dietitian = "api/dietitians";
+    public const string client = "api/clients";
+    public const string auth = "api/auth";
+    public const string meal = "api/meals";
+    public const string dietPlan = "api/diet-plans";
+    public const string progress = "api/progress";
+}
+
